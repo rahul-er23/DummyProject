5a917e5266fd12d63099d48000bfa7c48f8bda04
Rahul Ingle <rahul.er23@gmail.com>
2022-09-13 19:40:16 +0530

    used for each loop instead of iterator

--- DummyProject/src/com/rahul/git/ArrayDemo.java
+++ DummyProject/src/com/rahul/git/ArrayDemo.java
@@ -1,16 +1,15 @@
 package com.rahul.git;
 
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
 public class ArrayDemo {
 
 	public static void main(String[] args) {
 		List<String> list = Arrays.asList("Rahul","Amol","Shital","Satyam");
-		Iterator<String> itr = list.iterator();
-		while(itr.hasNext()) {
-			System.out.println(itr.next());
+		
+		for(String str:list) {
+		System.out.println(str);	
 		}
 
 		
