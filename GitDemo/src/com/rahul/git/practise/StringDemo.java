5f635a24f730a1d7ab92afc1b6727366374f9a14
Rahul Ingle <rahul.er23@gmail.com>
2022-09-14 12:39:55 +0530

    New project gitdemo & stringdemo Class added

---
+++ GitDemo/src/com/rahul/git/practise/StringDemo.java
@@ -0,0 +1,16 @@
+package com.rahul.git.practise;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		String s1="Rahul";
+		String s2="Ingle";
+		
+		StringBuffer sb=new StringBuffer("Velocity Traninee");
+		sb.append(s1);
+		sb.append(s2);
+		System.out.println(sb);
+
+	}
+
+}
